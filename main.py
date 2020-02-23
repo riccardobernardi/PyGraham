6c831abf825d7cbed9ca0b789aff06cec9d97473
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-02-23 12:37:33 +0100

    done for list

---
+++ main.py
@@ -0,0 +1,13 @@
+import functional
+
+v = functional.list([1,2,3])
+
+print(v.map(lambda x: 2*x))
+
+###################################
+
+from functional import *
+
+v = list([1,2,3])
+
+print(v.map(lambda x: 2*x))
